e089842d801800245ceda1eab1618017476700b4
FIC <FIC@info.local>
2021-02-12 08:17:06 -0300

    Projeto Inciado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace instadev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
